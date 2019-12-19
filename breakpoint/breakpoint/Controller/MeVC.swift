9680f265d883a8ebd8696cc801cc9892f549f418
Tomasz Klocek <tomek@klocek.info>
2019-12-19 13:49:17 +0100

    MeVC added

---
+++ breakpoint/breakpoint/Controller/MeVC.swift
@@ -0,0 +1,31 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Tomek Klocek on 2019-12-19.
+//  Copyright © 2019 Tomek Klocek. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        //Sign out code
+        
+    }
+    
+
+}
